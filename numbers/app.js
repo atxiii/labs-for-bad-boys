cd1ce9a68fad3973c86d8a90dca8b6134278a8f2
Hosein <hoseinshurabi@gmail.com>
2024-05-10 09:13:22 +0330

    Friday-09-13 update numbers lab

---
+++ numbers/app.js
@@ -0,0 +1,11 @@
+x='a'
+xx=parseFloat(x);
+if(xx==0 && !x.startsWith('0') alert('flag 1');
+else if(xx>0){
+		if(xx==xx-1){
+			alert('flag 2')
+		}
+}else {
+		if(xx!=xx) alert('flag 3');
+		else (!(xx>=0)) alert('flag 4');
+}
